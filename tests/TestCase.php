79aa933f2162b9c5fb86a0a3f4f0854226bc45df
dibakalantari <dibakalantari97@gmail.com>
2024-07-05 09:31:16 +0100

    WIP: Experiment testing with 2 different database connections

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,32 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
+use Illuminate\Support\Facades\DB; // Add this line
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+        $app->make(Kernel::class)->bootstrap();
+
+        $app->make(Kernel::class)->bootstrap();
+
+        $environmentFile = env('APP_ENV_FILE', '.env');
+
+        if (file_exists($file = $app->environmentPath() . '/' . $environmentFile)) {
+            $app->loadEnvironmentFrom($environmentFile);
+        }
+
+        return $app;
+    }
+
+    // public function changeConnection($connection)
+    // {
+    //     DB::setDefaultConnection($connection); 
+
+
+    // }
+
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,50 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_switches_database_connections_within_a_single_test()
     {
-        $this->assertTrue(true);
+        DB::setDefaultConnection('sqlite');
+
+        $sqliteUser = User::create([
+            'name' => 'John',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $sqliteUser->id,
+            'name' => 'John',
+        ], 'sqlite');
+
+        DB::setDefaultConnection('mysql');
+
+        $mysqlUser = User::create([
+            'name' => 'Jack',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $mysqlUser->id,
+            'name' => 'Jane Smith',
+        ], 'mysql');
+
+        DB::setDefaultConnection('sqlite');
+
+        $anotherSqliteUser = User::create([
+            'name' => 'Mark Brown',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $anotherSqliteUser->id,
+            'name' => 'Mark Brown',
+        ], 'sqlite');
     }
 }
+
